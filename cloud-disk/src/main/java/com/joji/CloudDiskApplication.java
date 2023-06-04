af58146f525da1f973980f5768137921f07e3962
YangZhiXiong <641484973@qq.com>
2023-06-04 20:39:42 +0800

    Spring启动类

--- cloud-disk/src/main/java/com/joji/CloudDiskApplication.java
+++ cloud-disk/src/main/java/com/joji/CloudDiskApplication.java
@@ -1,5 +1,11 @@
 package com.joji;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CloudDiskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDiskApplication.class,args);
+    }
 }
