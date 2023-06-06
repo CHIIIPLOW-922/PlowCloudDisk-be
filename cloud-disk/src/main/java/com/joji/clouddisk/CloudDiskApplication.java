9f51c059059b1ca98f122a0444890fa6c6a10735
YangZhiXiong <641484973@qq.com>
2023-06-06 15:46:05 +0800

    Springboot项目建立

--- cloud-disk/src/main/java/com/joji/clouddisk/CloudDiskApplication.java
+++ cloud-disk/src/main/java/com/joji/clouddisk/CloudDiskApplication.java
@@ -3,6 +3,9 @@ package com.joji.clouddisk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author YangZhiXiong
+ */
 @SpringBootApplication
 public class CloudDiskApplication {
 
